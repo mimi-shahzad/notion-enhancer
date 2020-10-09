4e237b4b51039cb7d5a7045375ac213656dd7b2f
Mimi Shahzad <mimi.shahzad@virginpulse.com>
2020-10-09 11:22:51 -0400

    FairyFloss Mod
    Based on Sailor Mercury's FairyFloss Theme from https://sailorhg.github.io/fairyfloss/

---
+++ mods/fairyfloss/mod.js
@@ -0,0 +1,21 @@
+/*
+ * fairyfloss
+ * (c) 2020 dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
+ * (c) 2020 u/mimi-shahzad
+ * under the MIT license
+ */
+
+'use strict';
+
+module.exports = {
+    id: '033bff54-50ba-4cec-bdc0-b2ca7e307088',
+    tags: ['theme', 'dark'],
+    name: 'fairyfloss',
+    desc: 'a theme based on fairyfloss by sailor-mercury ',
+    version: '0.1',
+    author: {
+        name: 'mimi-shahzad',
+        link: 'https://github.com/mimi-shahzad',
+        avatar: 'https://secure.gravatar.com/avatar/86c98dec3a06245dbf2291deb44441f5',
+    }
+};
